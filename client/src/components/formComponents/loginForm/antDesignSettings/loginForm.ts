ee275a11c98c3ee868622654a035efcaff2d82f2
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-12-16 15:26:33 +0300

    Refactor antDesignSettings for login

--- client/src/components/formComponents/loginForm/antDesignSettings/loginForm.ts
+++ client/src/components/formComponents/loginForm/antDesignSettings/loginForm.ts
@@ -1,11 +1,21 @@
-export const RULES = {
-  id: [
-    { required: true, message: 'Укажите ваш ID' }
-  ]
+interface ISettings {
+  id: string;
+}
+
+const SETTINGS_RU = {
+  id: 'Укажите ваш ID'
 };
 
-export const RULES_EN = {
-  id: [
-    { required: true, message: 'Please enter your ID' }
-  ]
+const SETTINGS_EN = {
+  id: 'Please enter your ID'
 };
+
+const getRules = (settings: ISettings) => ({
+  id: [
+    { required: true, message: settings.id }
+  ]
+});
+
+export const RULES = getRules(SETTINGS_RU);
+
+export const RULES_EN = getRules(SETTINGS_EN);
