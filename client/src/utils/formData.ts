820edd3614180747d8348347154287b04dc7a3e4
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-12-16 15:45:34 +0300

    Refactor formData util.

--- client/src/utils/formData.ts
+++ client/src/utils/formData.ts
@@ -1,8 +1,11 @@
 import { IMGBB_API_CONFIG } from '../api/imgbbApi/config';
 
+const KEY_FILED = 'key';
+const IMAGE_FIELD = 'image';
+
 export const setFormData = (data: any) => {
   const formData = new FormData();
-  formData.set('key', IMGBB_API_CONFIG.key);
-  formData.set('image', data.result.replace(/^.*,/, ''));
+  formData.set(KEY_FILED, IMGBB_API_CONFIG.key);
+  formData.set(IMAGE_FIELD, data.result.replace(/^.*,/, ''));
   return formData;
 };
