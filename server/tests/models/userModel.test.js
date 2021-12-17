82e7c10622325588df857e9a2ecc34c819d55995
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-12-17 21:24:35 +0300

    Add test for user route.

--- server/tests/models/userModel.test.js
+++ server/tests/models/userModel.test.js
@@ -1,51 +1,11 @@
 const model = require('../../src/models/userModel');
+const user = require('../../mocks/user');
 
-const DATUM = {
-  id: '60d0fe4f5311236168a109ce',
-  title: 'mr',
-  firstName: 'Rudi',
-  lastName: 'Droste',
-  picture: 'https://randomuser.me/api/portraits/med/men/83.jpg',
-  gender: 'male',
-  email: 'rudi.droste@example.com',
-  dateOfBirth: '2021-06-21T21:02:08.029Z',
-  phone: '0421-7342787',
-  location: {
-    street: '1196, MÃ¼hlenweg',
-    city: 'GrÃ¼nhain-Beierfeld',
-    state: 'Sachsen-Anhalt',
-    country: 'Germany',
-    timezone: '+3:30'
-  },
-  registerDate: '2021-06-21T21:02:08.029Z',
-  updatedDate: '2021-06-21T21:02:08.029Z'
-};
-
-const RESULT = {
-  id: '60d0fe4f5311236168a109ce',
-  firstName: 'Rudi',
-  lastName: 'Droste',
-  title: 'mr',
-  gender: 'male',
-  email: 'rudi.droste@example.com',
-  phone: '0421-7342787',
-  dateOfBirth: {
-    enDate: '22 of June 2021',
-    ruDate: '22 Июня 2021 года',
-    enDateAndTime: '22 of June 00:02',
-    ruDateAndTime: '22 Июня 00:02'
-  },
-  registerDate: {
-    enDate: '22 of June 2021',
-    ruDate: '22 Июня 2021 года',
-    enDateAndTime: '22 of June 00:02',
-    ruDateAndTime: '22 Июня 00:02'
-  },
-  picture: 'https://randomuser.me/api/portraits/med/men/83.jpg'
-};
+const datum = user.userMockData;
+const result = user.userProcessedMockData;
 
 describe('User Model', () => {
   it('parseDatum should return only necessary processed fields', () => {
-    expect(model.parseDatum(DATUM)).toEqual(RESULT);
+    expect(model.parseDatum(datum)).toEqual(result);
   })
 })
--- server/tests/routes/userRouter.test.js
+++ server/tests/routes/userRouter.test.js
@@ -1,34 +1,48 @@
 const request = require('supertest');
 const server = require('../../src/server');
 const { mock } = require('../../mocks/axios');
+const user = require('../../mocks/user');
+const constants = require('../../mocks/constants');
 
 describe('User Router', () => {
   it('getUserList should return user list', async () => {
     mock.onGet('/user')
-      .replyOnce(200,
-        JSON.stringify({ data: [], page: 0, limit: 5, total: 100 })
-      );
+      .replyOnce(constants.successStatus, JSON.stringify(user.userListMockData));
 
     const result = await request(server)
-      .get('/proxy/user')
-      .send();
+      .get('/proxy/user').send();
 
-    expect(result.statusCode).toBe(200);
-    expect(result.text)
-      .toEqual(
-        JSON.stringify({ data: { data: [], page: 0, limit: 5, total: 100 } })
-      );
+    expect(result.statusCode).toBe(constants.successStatus);
+    expect(result.text).toEqual(JSON.stringify({ data: user.userListMockData }));
   });
 
   it('getUserList should return error', async () => {
-    mock.onGet('/user')
-      .replyOnce(520);
+    mock.onGet('/user').replyOnce(constants.errorStatus);
 
-    const result = await request(server)
-      .get('/proxy/user')
-      .send();
+    const result = await request(server).get('/proxy/user').send();
 
-    expect(result.statusCode).toBe(520);
-    expect(JSON.parse(result.text).message).toBe('Request failed with status code 520');
+    expect(result.statusCode).toBe(constants.errorStatus);
+    expect(JSON.parse(result.text).message).toBe(constants.errorText);
+  })
+
+  it('getUserById should return user', async () => {
+    mock
+      .onGet(/\/user\/\w+/)
+      .replyOnce(constants.successStatus,
+        JSON.stringify(user.userMockData));
+
+    const result = await request(server).get('/proxy/user/12345').send();
+
+    expect(result.statusCode).toBe(constants.successStatus);
+    expect(result.text).toEqual(JSON.stringify({ data: user.userProcessedMockData }));
+  });
+
+  it('getUserById should return error', async () => {
+    mock.onGet(/\/user\/\w+/).replyOnce(constants.errorStatus);
+
+    const result = await request(server).get('/proxy/user/12345').send();
+
+    expect(result.statusCode).toBe(constants.errorStatus);
+    expect(JSON.parse(result.text).message).toBe(constants.errorText);
   })
 });
