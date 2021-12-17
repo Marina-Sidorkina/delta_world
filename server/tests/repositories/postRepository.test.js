8d6d1ab6ac6044f33a828e373aee3a4230e5f932
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-12-17 04:04:22 +0300

    Add fixes.

--- server/tests/repositories/postRepository.test.js
+++ server/tests/repositories/postRepository.test.js
@@ -2,16 +2,16 @@ const { getPostsListMockSuccess, getPostsListMockError } = require('../mocks/axi
 const repository = require('../../src/repositories/postRepository')
 
 describe('Post Repository', () => {
-  it('getPostsList should return resolved', () => {
+  it('getPostsList should return resolve with data', () => {
     getPostsListMockSuccess();
 
     repository.getPostsList()
       .then((response) => {
-        expect(response.postsList).toEqual('Value');
+        expect(response.postsList).toBe('Value');
       });
   });
 
-  it('getPostsList should return rejected', () => {
+  it('getPostsList should return reject with error', () => {
     getPostsListMockError();
 
     repository.getPostsList()
