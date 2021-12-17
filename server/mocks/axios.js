1e123dc970a7a72b5b16009b8339f1831084d546
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-12-17 23:24:29 +0300

    Refactor postRepository tests.

--- server/mocks/axios.js
+++ server/mocks/axios.js
@@ -5,30 +5,6 @@ const mock = new MockAdapter(dummyApi);
 
 module.exports =  {
   mock,
-  getPostsListMockSuccess: () => mock
-    .onGet('/post', { params: { page: 0, limit: 5 } })
-    .replyOnce(200, JSON.stringify({ data: [], page: 0, limit: 5, total: 100 })),
-  getPostsListMockError: () => mock
-    .onGet('/post', { params: { page: 0, limit: 5 } })
-    .replyOnce(520),
-  getPostByIdMockSuccess: () => mock
-    .onGet(/\/post\/\w+/)
-    .replyOnce(200,
-      JSON.stringify({ id: '12345', image: 'image', owner: {}, text: 'text' })),
-  getPostByIdMockError: () => mock
-    .onGet(/\/post\/\w+/)
-    .replyOnce(520),
-  getPostCommentsListMockSuccess: () => mock
-    .onGet(/\/post\/\w+\/comment/, { params: { page: 0, limit: 1 } })
-    .replyOnce(200,
-      JSON.stringify({ data: [
-        { id: '12345', message: 'message', owner: {},
-          post: '12345', publishDate: '2020-03-06T23:00:40.972Z'}
-        ],
-        page: 0, limit: 1, total: 100 })),
-  getPostCommentsListMockError: () => mock
-    .onGet(/\/post\/\w+\/comment/, { params: { page: 0, limit: 1 } })
-    .replyOnce(520),
   updateUserByIdMockSuccess: () => mock
     .onPut(/\/user\/\w+/, { firstName: 'TEST10', lastname: 'TEST10' })
     .replyOnce(200, JSON.stringify({ id: '12345', firstName: 'TEST10', lastName: 'TEST10', title: 'ms',
--- server/tests/repositories/postRepository.test.js
+++ server/tests/repositories/postRepository.test.js
@@ -1,64 +1,68 @@
-const {
-  getPostsListMockSuccess, getPostsListMockError, getPostByIdMockSuccess,
-  getPostByIdMockError, getPostCommentsListMockSuccess, getPostCommentsListMockError
-} = require('../../mocks/axios');
+const { mock } = require('../../mocks/axios');
 const repository = require('../../src/repositories/postRepository');
+const post = require('../../mocks/post');
+const constants = require('../../mocks/constants');
 
 describe('Post Repository', () => {
   it('getPostsList should return resolve with data', () => {
-    getPostsListMockSuccess();
+    mock
+      .onGet('/post', { params: { page: 0, limit: 5 } })
+      .replyOnce(constants.successStatus, JSON.stringify(post.postListMockData));
 
-    repository.getPostsList(0, 5)
+      repository.getPostsList(0, 5)
       .then((response) => {
-        expect(response).toStrictEqual({ data: [], page: 0, limit: 5, total: 100 });
+        expect(response).toStrictEqual(post.postListMockData);
       });
   });
 
   it('getPostsList should return reject with error', () => {
-    getPostsListMockError();
+    mock.onGet('/post', { params: { page: 0, limit: 5 } })
+      .replyOnce(constants.errorStatus);
 
     repository.getPostsList(0, 5)
       .catch((error) => {
-        expect(error.message).toBe('Request failed with status code 520');
+        expect(error.message).toBe(constants.errorText);
       });
   });
 
   it('getPostById should return resolve with data', () => {
-    getPostByIdMockSuccess();
+    mock.onGet(/\/post\/\w+/)
+      .replyOnce(constants.successStatus,
+        JSON.stringify(post.postMockData));
 
-    repository.getPostById('12345')
+      repository.getPostById('12345')
       .then((response) => {
-        expect(response).toStrictEqual({ id: '12345', image: 'image', owner: {}, text: 'text' });
+        expect(response).toStrictEqual(post.postMockData);
       });
   });
 
   it('getPostById should return reject with error', () => {
-    getPostByIdMockError();
+    mock.onGet(/\/post\/\w+/).replyOnce(constants.errorStatus);
 
-    repository.getPostById('12345')
+      repository.getPostById('12345')
       .catch((error) => {
-        expect(error.message).toBe('Request failed with status code 520');
+        expect(error.message).toBe(constants.errorText);
       });
   });
 
   it('getPostCommentsList should return resolve with data', () => {
-    getPostCommentsListMockSuccess();
+    mock.onGet(/\/post\/\w+\/comment/, { params: { page: 0, limit: 1 } })
+      .replyOnce(constants.successStatus,
+        JSON.stringify(post.postCommentMockData));
 
     repository.getPostCommentsList('12345', 0, 1)
       .then((response) => {
-        expect(response).toStrictEqual({
-          data: [{id: '12345', message: 'message', owner: {}, post: '12345', publishDate: '2020-03-06T23:00:40.972Z'}],
-          page: 0, limit: 1, total: 100
-        });
+        expect(response).toStrictEqual(post.postCommentMockData);
       });
   });
 
   it('getPostCommentsList should return reject with error', () => {
-    getPostCommentsListMockError();
+    mock.onGet(/\/post\/\w+\/comment/, { params: { page: 0, limit: 1 } })
+      .replyOnce(constants.errorStatus);
 
-    repository.getPostCommentsList('12345', 0, 1)
+      repository.getPostCommentsList('12345', 0, 1)
       .catch((error) => {
-        expect(error.message).toBe('Request failed with status code 520');
+        expect(error.message).toBe(constants.errorText);
       });
   });
 });
