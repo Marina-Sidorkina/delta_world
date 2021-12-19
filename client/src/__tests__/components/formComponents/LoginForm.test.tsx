96884b98336cbb21fbfdc602723b7da38dfdd0c9
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-12-19 21:43:02 +0300

    Add test for LoginForm component.

---
+++ client/src/__tests__/components/formComponents/LoginForm.test.tsx
@@ -0,0 +1,41 @@
+import '../../../../mocks/matchMediaMock';
+import React from 'react';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { configure, mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import mockStore from "../../../../mocks/mockStore";
+import LoginForm from "../../../components/formComponents/loginForm/LoginForm";
+import {HashRouter} from "react-router-dom";
+
+jest.mock('../../../redux/actions/login');
+configure({ adapter: new Adapter() });
+
+describe('LoginForm component test', () => {
+  let store: any;
+  let wrapper: any;
+
+  beforeEach(() => {
+    store = mockStore({
+      languageSelector: {
+        value: 'ru'
+      },
+      login: {
+        data: {
+          error: false,
+          isLoading: false,
+          inputValue: ''
+        }
+      }
+    });
+    store.dispatch = jest.fn();
+    wrapper = mount(<Provider store={store}><HashRouter><LoginForm /></HashRouter></Provider>);
+  })
+
+  test('should render LoginForm', () => {
+    expect(wrapper.length).toEqual(1)
+  });
+
+  test('should render LoginForm', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
