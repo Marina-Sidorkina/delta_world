a4deef59ef5ef4291a02d4298785e83428b1612c
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-12-19 19:29:25 +0300

    Add test for LanguageSelect component.

---
+++ client/mocks/mockStore.ts
@@ -0,0 +1,7 @@
+import configureStore from 'redux-mock-store';
+import thunk from "redux-thunk";
+
+
+const mockStore = configureStore([thunk]);
+
+export default mockStore;
---
+++ client/src/__tests__/components/footerComponents/LanguageSelect.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { configure, mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import mockStore from "../../../../mocks/mockStore";
+import LanguageSelect from '../../../components/footerComponents/languageSelect/LanguageSelect';
+
+configure({ adapter: new Adapter() });
+
+describe('LanguageSelect component test', () => {
+  let store: any;
+  let wrapper: any;
+
+  beforeEach(() => {
+    store = mockStore({
+      languageSelector: {
+        value: 'ru'
+      }
+    });
+
+    wrapper = mount(<Provider store={store}><LanguageSelect /></Provider>);
+  })
+
+  test('should render LanguageSelect', () => {
+    expect(wrapper.length).toEqual(1)
+  });
+
+  test('prop should match with store', () => {
+    expect(wrapper.prop('languageSelector')).toEqual(store.languageSelector);
+  });
+
+  test('should render LanguageSelect', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
