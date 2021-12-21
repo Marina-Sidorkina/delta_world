cd528595da2576021dbe65abf4d411f16ab1222d
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-12-22 02:25:45 +0300

    Add test for login actions.

--- client/mocks/proxy.js
+++ client/mocks/proxy.js
@@ -1,6 +1,4 @@
 const MockAdapter = require('axios-mock-adapter');
 const { proxy } = require('../src/api/proxy/index');
 
-const mock = new MockAdapter(proxy);
-
-module.exports =  { mock };
+export const mock = new MockAdapter(proxy);
