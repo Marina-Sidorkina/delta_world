98b245354732def6269785d6c7a58128c44da98b
Marina Sidorkina <m.sidorkina55@gmail.com>
2021-12-22 16:58:35 +0300

    Add test for UserInfoEditIcon.

--- client/src/__tests__/components/userComponents/UserInfoEditIcon.test.tsx
+++ client/src/__tests__/components/userComponents/UserInfoEditIcon.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { configure, shallow } from 'enzyme';
 import UserInfoEditIcon from "../../../components/userComponents/userInfoEditIcon/UserInfoEditIcon";
+
 configure({ adapter: new Adapter() });
 
 describe('UserInfoEditIcon component test', () => {
